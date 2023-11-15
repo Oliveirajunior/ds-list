a62c9a1ae5ad9dc4fcb0dd7e2fd2662a8f679786
Manoel Junior <manuelitojunior@gmail.com>
2023-11-14 22:10:57 -0300

    Projection query and list

--- src/main/java/com/example/dslist/controllers/GameListController.java
+++ src/main/java/com/example/dslist/controllers/GameListController.java
@@ -19,9 +19,18 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDto> findAll(){
         List<GameListDto> result = gameListService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDto> findById(@PathVariable Long listId){
+        List<GameMinDto> result = gameService.findByList(listId);
         return result;
     }
 }
--- src/main/java/com/example/dslist/services/GameService.java
+++ src/main/java/com/example/dslist/services/GameService.java
@@ -3,6 +3,8 @@ package com.example.dslist.services;
 import com.example.dslist.dto.GameDto;
 import com.example.dslist.dto.GameMinDto;
 import com.example.dslist.entities.Game;
+import com.example.dslist.projections.GameMinProjection;
+import com.example.dslist.repositories.GameListRepository;
 import com.example.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,9 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Autowired
+    private GameListRepository gameListRepository;
+
     @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
         List<Game> result = gameRepository.findAll();
@@ -28,6 +33,14 @@ public class GameService {
         Game result = gameRepository.findById(id).get();
         return new GameDto(result);
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDto> findByList(Long listId) {
+        List<GameMinProjection> result = gameListRepository.searchByList(listId);
+        return result.stream().map(GameMinDto::new).toList();
+    }
+
+
 
 
 }
